638f7427f9c7894be800036b27f10ed074681cb9
Bruno Martorana <123768170+PYKYB@users.noreply.github.com>
2023-11-01 21:41:45 -0300

    Update Envios.js

--- Envios.js
+++ Envios.js
@@ -1,11 +1,11 @@
 //codigo Envios.js
+
+// Cargar variables de entorno desde el archivo .env
+require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 app.use(express.json());
-
-// Cargar variables de entorno desde el archivo .env
-require('dotenv').config();
 
 const connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -26,7 +26,7 @@ connection.connect((err) => {
 app.post('/registro', (req, res) => {
     const { nombre, apellido, email, contrasena, cuil } = req.body;
 
-    const query = 'INSERT INTO users (nombre, apellido, username, email, contrasena, cuil) VALUES (?, ?, ?, ?, ?, ?)';
+    const query = 'INSERT INTO users (nombre, apellido, email, contrasena, cuil) VALUES (?, ?, ?, ?, ?)';
     connection.query(query, [nombre, apellido, email, contrasena, cuil], (err, results) => {
         if (err) {
             console.error('Error al insertar datos en la base de datos:', err);
@@ -38,7 +38,19 @@ app.post('/registro', (req, res) => {
     });
 });
 
-const PORTme = 3000;
-app.listen(PORTme, () => {
+const PORT = 3000;
+app.listen(PORT, () => {
     console.log(`Servidor en ejecución en el puerto ${PORT}`);
+});
+
+// Cerrar la conexión a la base de datos al finalizar la aplicación
+process.on('SIGINT', () => {
+    connection.end((err) => {
+        if (err) {
+            console.error('Error al cerrar la conexión:', err);
+        } else {
+            console.log('Conexión cerrada correctamente');
+        }
+        process.exit(0);
+    });
 });
